20a9520ebf06e233167ac112724de9b53af7acf1
Jomstaid <jomaralcantara0828@gmail.com>
2025-08-15 01:07:16 +0800

    first commit

---
+++ Java/1. Syntax/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.print("No World");
+        System.out.println("Yo");
+        
+        System.out.println(10.000);
+        System.out.println(9);
+        System.out.println(9 + 9);
+        System.out.println(10 - 1);
+        System.out.println(2 * 9);
+        System.out.println(10 / 2);
+
+        
+    }
+}
---
+++ Java/2. Comment/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // String
+        System.out.println("Hi");
+        System.out.print("No World");
+        System.out.println("Yo");
+        
+        // Integers
+        System.out.println(10.000);
+        System.out.println(9);
+        System.out.println(9 + 9);
+        System.out.println(10 - 1);
+        System.out.println(2 * 9);
+        System.out.println(10 / 2);
+
+        /* Multi 
+        Comment */
+    }
+}
