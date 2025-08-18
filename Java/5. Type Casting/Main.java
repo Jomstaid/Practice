890addb3d0f8a4b25b02641699afc60f241d836f
Jomstaid <jomaralcantara0828@gmail.com>
2025-08-18 19:57:53 +0800

    4th Day

---
+++ Java/5. Type Casting/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // Widening Casting
+        int num = 10;
+        double decimal = num;
+
+        System.out.println(num);
+        System.out.println(decimal);
+
+
+
+        // Narrowing Casting
+        double decimal1 = 10.501d;
+        int num2 = (int) decimal1;
+
+        System.out.println(decimal1);
+        System.out.println(num2);
+
+
+
+
+
+
+
+    }
+
+}
